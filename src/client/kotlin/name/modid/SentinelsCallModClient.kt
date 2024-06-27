b443c430312c36b73aceb40879ca9d5a62f37e2d
AndroAGENT <andrija.misic.yt@gmail.com>
2024-06-27 17:08:36 +0200

    Update

---
+++ src/client/kotlin/name/modid/SentinelsCallModClient.kt
@@ -0,0 +1,9 @@
+package name.modid
+
+import net.fabricmc.api.ClientModInitializer
+
+object SentinelsCallModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/name/modid/SentinelsCallModDataGenerator.kt
@@ -0,0 +1,9 @@
+package name.modid
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object SentinelsCallModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
